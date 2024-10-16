070eb3845baafd7d9676fd024c028177ff8cae27
Bertitom <juanbertoletti2@gmail.com>
2024-10-16 21:31:10 +0000

    Haciendo un contador

---
+++ src/js/component/secondsCounter.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Card = ({digito}) => {
+	return (
+		<div className="card-body bg-black">
+		  <h5 className="card-title">{digito}</h5>
+		</div>
+	);
+  };
+
+  const SecondsCounter = (props) => {
+    return (
+        <div className="d-flex" >
+            <div className="card-body bg-black">
+              <i className="far fa-clock"></i>
+            </div>
+            <Card digito={Math.floor(props.counter/100000 %10)} />
+            <Card digito={Math.floor(props.counter/10000 %10)} />
+            <Card digito={Math.floor(props.counter/1000 %10)} />
+            <Card digito={Math.floor(props.counter/100 %10)} />
+            <Card digito={Math.floor(props.counter/10 %10)} />
+            <Card digito={props.counter %10} />
+        </div>
+    )
+  }
+
+  
+
+  
+  export {Card, SecondsCounter};
